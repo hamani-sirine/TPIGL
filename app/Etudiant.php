9fc6f454056d51d8d519adf0897ff78aaf6d8ffe
Hamani Sirine <47010713+hamani-sirine@users.noreply.github.com>
2019-11-19 15:36:05 +0100

    models

---
+++ app/Etudiant.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Etudiant extends Model
+{
+    protected $table='etudiant';
+    protected $primaryKey = 'id_etud';
+
+}
--- app/User.php
+++ app/User.php
@@ -9,15 +9,9 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     use Notifiable;
+    protected $table='user';
+    protected $primaryKey = 'id_user';
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
 
     /**
      * The attributes that should be hidden for arrays.
@@ -25,15 +19,9 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'mdp',
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    
+  
 }
