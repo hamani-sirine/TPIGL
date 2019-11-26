6bb089b5f7abc1cd85c844f63ea2f15fd9cabe9c
Hamani Sirine <47010713+hamani-sirine@users.noreply.github.com>
2019-11-26 17:11:34 +0100

    ajout des routes api

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,20 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+/*
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-});
+}); */
+
+Route::get('/etud',"EtudiantController@create");
+Route::get('/cr',"EtudiantController@Ajouter_etudiant");
+Route::get('/viewst',"EtudiantController@ViewStud");
+
+Route::get('/admin',"AdminController@create");
+Route::get('/cr2',"AdminController@Ajouter_admin");
+
+Route::get('/ens',"EnseignantController@create");
+Route::get('/cr3',"EnseignantController@Ajouter_enseignant");
+
+
+Route::get('/notes/{id}','NoteController@index')->name('notes');
