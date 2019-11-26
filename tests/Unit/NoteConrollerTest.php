f64e2a1f0245f2d9b178b18f7064b28f234d003b
Hamani Sirine <47010713+hamani-sirine@users.noreply.github.com>
2019-11-26 21:48:57 +0100

    ajout de quelques commentaires

--- tests/Unit/NoteConrollerTest.php
+++ tests/Unit/NoteConrollerTest.php
@@ -8,9 +8,18 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 use Tests\TestCase;
+///Unit test class for NoteController.
+///
+///This class is used to test the fonctionality "view student's grades".    
 
 class NoteConrollerTest extends TestCase
-{
+{       
+    /**
+    * This fonction  checks if the data passed to the view "notes" exists and is correct.
+    *
+    * @return void
+    */
+     
    
     public function testIndex()
 	{  $i=1;
@@ -20,7 +29,12 @@ class NoteConrollerTest extends TestCase
 
 
     }
-    
+    /**
+     * This function checks if all the parameters are NULL in the case where the record doesn't exist and the data wasn't sent to the view.
+     *
+     * @return void
+     */
+
     public function testIndex2()
 	{  
     $response = $this->call('GET', 'notes/1000');
